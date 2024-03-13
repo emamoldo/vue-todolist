5e78da5cc5ca7c4c57e6d352fd014c7d1cbc1271
emamoldo <moldovanemanuel92.em@gmail.com>
2024-03-13 19:44:06 +0100

    Add the ul and the todo list input with the js part

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const { createApp } = Vue 
+createApp({
+    data() {
+        return{
+            newTask: '',
+            tasks: [
+                
+            ]
+        }
+    }
+}).mount('#app')
